bb8f28a345f9d9f20b246d2828ff92cac6ed6ee7
Johnson Mao <tutelary.maomao@gmail.com>
2023-06-17 21:43:08 +0800

    feat: deck style

--- frontend/components/decks.vue
+++ frontend/components/decks.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="decks">
-    decks
+    <IncanGoldCard
+      v-for="index in 7"
+      :key="index"
+      class="decks__card"
+      :style="{
+        '--index': `${index * - 3}px`,
+      }"
+    />
   </div>
 </template>
 
 <style>
 .decks {
-  width: 170px;
-  height: 210px;
-  background-color: transparent;
-  color: white;
+  position: relative;
+  margin-right: 28px;
+  width: 144px;
+  height: 100%;
+}
+.decks__card {
+  position: absolute;
+  top: 50%;
+  left: 28px;
+  width: 100%;
+  transform: translate(calc(var(--index)), calc(-50% + 5px));
 }
 </style>
