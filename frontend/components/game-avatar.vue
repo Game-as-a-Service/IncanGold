7d5264b0bc43ebca0fcc838900cffaff133c0486
Peggy Lin <stu90233@gmail.com>
2023-06-17 08:51:54 +0800

    add: game avatar

--- frontend/components/game-avatar.vue
+++ frontend/components/game-avatar.vue
@@ -1,31 +1,205 @@
 <template>
-	<div class="game-avatar">
+	<div :class="['game-avatar', { 'game-avatar__empty': emptyPlayer }]">
 		<div class="avatar">
-			<div class="img"></div>
-			<div class="turn-score"></div>
-			<div class="total-score"></div>
+			<img
+				v-if="!round && inTunnel"
+				:src="emptyPlayer ? '../public/game-avatar__empty.png' : 'playerAvatar'"
+				:alt="avatar" />
+			<div v-else class="avatar-empty">
+				<img src="" alt="" />
+				<p>0</p>
+			</div>
+			<div class="name">
+				<p>{{ playerName }}</p>
+			</div>
+			<div class="tent-score">{{ tentScore }}</div>
 		</div>
-		<p>player name</p>
-		<div class="ready"></div>
+
+		<div
+			v-if="!playerIsHost && !emptyPlayer"
+			:class="[
+				'game-avatar__status',
+				{ 'game-avatar__status--ready': playerIsReady },
+			]"></div>
+		<div v-if="playerIsHost" class="playerIsHost">
+			<p>房主</p>
+		</div>
 	</div>
 </template>
 
+<!-- 版型
+<template>
+	<div class="game-avatar">
+		<div class="avatar">
+			<img src="../public/avatar1.png" alt="avatar" />
+			<div class="avatar-empty">
+				<img src="" alt="" />
+				<p>0</p>
+			</div>
+			<div class="name">
+				<p>player name</p>
+			</div>
+			<div class="total-score">0</div>
+		</div>
+
+		<div class="game-avatar__status game-avatar__status--ready"></div>
+
+		<div class="playerIsHost">
+			<p>房主</p>
+		</div>
+	</div>
+</template>
+-->
+
+<script setup>
+	import { compose, defaultTo, isEmpty, isNil, path } from "ramda";
+
+	const props = defineProps({
+		user: {
+			type: Object,
+			default: () => ({}),
+		},
+	});
+
+	const { user } = toRefs(props);
+
+	const emptyPlayer = computed(() => {
+		return isNil(user.value) || isEmpty(user.value);
+	});
+
+	const playerIsHost = computed(() => {
+		return path(["host"], user.value);
+	});
+	// const playerIsHost = true;
+
+	const playerName = computed(() => {
+		return path(["name"], user.value);
+	});
+
+	const playerAvatar = computed(() => {
+		return compose(defaultTo("./player.png"), path(["avatar"]))(user.value);
+	});
+
+	const playerIsReady = computed(() => {
+		return path(["avatar"], user.value) === "ready";
+	});
+</script>
+
 <style>
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+	}
+
 	.game-avatar {
 		width: 158px;
-		border-radius: 99rem;
-		background-color: gray;
+		border-radius: 1.25rem;
+		background-color: rgba(228, 228, 228, 0.4);
 		position: relative;
+		margin-bottom: 10px;
+		overflow: hidden;
+	}
+
+	.game-avatar:last-child {
+		margin-bottom: 0;
+	}
+
+	.avatar {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-left: 23px;
+		padding: 20px 20px 10px 6px;
+	}
+
+	.game-avatar img {
+		width: 118px;
+		height: 118px;
+		border: 1px solid #939393;
+		border-radius: 100px;
+		margin-bottom: 12px;
 	}
 
 	.game-avatar p {
+		font-size: 18px;
+		color: #fff;
+		margin: 0;
+	}
+
+	/* 遊戲進行中 */
+	.avatar-empty {
+		position: relative;
+	}
+
+	.avatar-empty img {
+		background-color: #d9d9d9;
+	}
+
+	.avatar-empty p {
 		position: absolute;
-		font-size: 16px;
-		width: 100%;
+		top: 50%;
 		left: 50%;
-		bottom: -16px;
-		transform: translateX(-50%);
+		transform: translate(-50%, -55%);
+		font-size: 66px;
+		color: #7b7b7b;
+	}
+
+	.game-avatar__status {
+		position: absolute;
+		width: 17px;
+		height: 183px;
+		background-color: #bebdbd;
+		top: 0;
+		left: 0;
+	}
+
+	.game-avatar__status--ready {
+		background-color: rgba(44, 220, 0, 0.65);
+	}
+
+	/* 無玩家 */
+	.game-avatar__empty {
+		background-color: transparent;
+	}
+
+	.game-avatar__empty .game-avatar__status--ready {
+		background: transparent;
+	}
+
+	.game-avatar__empty img {
+		border: none;
+	}
+
+	/* 房主標示' */
+	.playerIsHost {
+		position: absolute;
+		top: 12px;
+		right: 0;
+		background-color: rgba(47, 47, 47, 0.7);
+		padding: 0 12px 0 10px;
+		border-radius: 10px 0 0 10px;
+	}
+
+	/* turn 分數 */
+
+	/* tent-score 分數 */
+	.tent-score {
+		width: 40px;
+		height: 40px;
+		position: absolute;
+		top: 100px;
+		right: 3px;
+
+		font-size: 25px;
+		font-weight: 700;
+		color: #bf0000;
 		text-align: center;
-		margin: 0px;
+		line-height: 38px;
+
+		background-color: #fff;
+		border: 2px solid #7b7b7b;
+		border-radius: 100px;
 	}
 </style>
--- frontend/components/incan-gold-game.vue
+++ frontend/components/incan-gold-game.vue
@@ -1,65 +1,76 @@
 <script setup>
-import take from 'ramda/es/take'
-import takeLast from 'ramda/es/takeLast'
+	import take from "ramda/es/take";
+	import takeLast from "ramda/es/takeLast";
 
-const player = ref([{},{},{},{},{},{},{},{}])
+	const player = ref([{}, {}, {}, {}, {}, {}, {}, {}]);
 
-const fistPartsOfPlayer = computed(() => {
-  return take(4, player.value)
-})
+	const fistPartsOfPlayer = computed(() => {
+		return take(4, player.value);
+	});
 
-const otherPartsOfPlayer = computed(() => {
-  return takeLast(4, player.value)
-})
+	const otherPartsOfPlayer = computed(() => {
+		return takeLast(4, player.value);
+	});
 </script>
 
 <template>
-  <div class="incan-gold-game">
-    <div class="incan-gold-game__top">
-      <div><decks /></div>
-      <div><tunnel /></div>
-      <div><temple /></div>
-    </div>
-    <div class="incan-gold-game__main">
-      <div class="incan-gold-game__avatars">
-        <template v-for="(player, indexOfPlayer) in fistPartsOfPlayer" :key="indexOfPlayer">
-          <game-avatar :user="player" />
-        </template>
-      </div>
-      <div>
-        <game-sceen />
-      </div>
-      <div class="incan-gold-game__avatars">
-        <template v-for="(player, indexOfPlayer) in fistPartsOfPlayer" :key="indexOfPlayer">
-          <game-avatar :user="player" />
-        </template>
-      </div>
-    </div>
-  </div>
+	<div class="incan-gold-game">
+		<div class="incan-gold-game__top">
+			<div><decks /></div>
+			<div><tunnel /></div>
+			<div><temple /></div>
+		</div>
+		<div class="incan-gold-game__main">
+			<div class="incan-gold-game__avatars">
+				<template
+					v-for="(player, indexOfPlayer) in fistPartsOfPlayer"
+					:key="indexOfPlayer">
+					<game-avatar :user="player" />
+				</template>
+			</div>
+			<div>
+				<game-sceen />
+			</div>
+			<div class="incan-gold-game__avatars">
+				<template
+					v-for="(player, indexOfPlayer) in fistPartsOfPlayer"
+					:key="indexOfPlayer">
+					<game-avatar :user="player" />
+				</template>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style>
-.incan-gold-game {
-  width: 1440px;
-  height: 1080px;
-}
+	.incan-gold-game {
+		width: 1440px;
+		height: 1080px;
+		background-color: #0e0a31;
+		background-image: linear-gradient(
+			rgba(248, 248, 248, 0.11),
+			rgba(248, 248, 248, 0.11)
+		);
+		margin: 0 auto;
+	}
 
-.incan-gold-game__top {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 10px;
-}
+	.incan-gold-game__top {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-bottom: 10px;
+	}
 
-.incan-gold-game__main {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-}
+	.incan-gold-game__main {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
 
-.incan-gold-game__avatars {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
+	.incan-gold-game__avatars {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		margin: 10px;
+	}
 </style>
