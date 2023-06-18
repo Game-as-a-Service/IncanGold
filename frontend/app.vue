94517ae9fbd4515480f0b9148cc4625433ff9b63
frohman <126720593+frohman-chang@users.noreply.github.com>
2023-06-18 14:21:11 +0800

    [功能開發] 加入文字設定 (#17)
    * feat: 🎸 add font family
    
    cubic use woff2 file. others use google font link.
    
    * fix: 🐛 update with figma
    
    * fix: 🐛 font cubic

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,3 +5,31 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+html, body, div#__nuxt {
+  font-family: 'Noto Sans';
+}
+
+@font-face {
+  font-family: 'Jokerman';
+  src: url(Jokerman.ttf);
+}
+
+@font-face {
+  font-family: 'Cubic 11';
+  src: url(Cubic_11_1.013_R.woff2);
+}
+
+.font-NotoSans {
+  font-family: "Noto Sans", "sans-serif", system-ui;
+}
+
+.font-cubic {
+  font-family: "Cubic 11", "sans-serif", system-ui;
+}
+
+.font-jokerman {
+  font-family: "Jokerman", "sans-serif", system-ui;
+}
+</style>
