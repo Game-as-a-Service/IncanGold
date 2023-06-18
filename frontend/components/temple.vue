61d8be785d82994b65cd06a6416053ff55dccff2
Peggy Lin <stu90233@gmail.com>
2023-06-18 12:45:32 +0800

    add: temple

--- frontend/components/temple.vue
+++ frontend/components/temple.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="temple">temple</div>
+	<div class="temple">
+		<img src="../public/temple.png" alt="temple" />
+	</div>
 </template>
 
 <style>
-.temple {
-  width: 170px;
-  height: 210px;
-  background-color: gray;
-  background-color: transparent;
-  color: white;
-}
+	.temple {
+		width: 163px;
+		height: 163px;
+		margin: 30px 20px;
+		text-align: center;
+	}
+
+	.temple img {
+		width: 137px;
+		height: 147px;
+	}
 </style>
