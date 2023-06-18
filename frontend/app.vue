996b26b32cc0849de0a9f493b89b3e6d2c41c84e
frohman <frohman.c.123@gmail.com>
2023-06-18 11:57:30 +0800

    feat: 🎸 add font family
    cubic use woff2 file. others use google font link.

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.font-k2d {
+  font-family: "K2D", "sans-serif", system-ui;
+}
+
+.font-lexend {
+  font-family: "Lexend", "sans-serif", system-ui;
+}
+
+.font-cubic {
+  font-family: "Cubic 11", "sans-serif", system-ui;
+}
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['normalize.css/normalize.css']
+  css: ['normalize.css/normalize.css'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800;900&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Lexend:wght@300;400;500;600;700;800;900&display=swap'
+        },
+        {
+          rel: 'stylesheet',
+          href: '/Cubic_11_1.013_R.woff2'
+        }
+      ]
+    }
+  }
 })
