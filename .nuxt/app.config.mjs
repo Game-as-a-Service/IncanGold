7d38d080014dffde106cc98f02cafcb31004eefe
frohman <frohman.c.123@gmail.com>
2023-06-18 13:27:12 +0800

    fix: 🐛 remove .nuxt

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'D:/01 程式設計/10 - GaaS/IncanGold/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
