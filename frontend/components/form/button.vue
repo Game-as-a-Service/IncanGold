3aeeea54333882eb9abc9359a64e99d7d0206bf2
Peggy Lin <stu90233@gmail.com>
2024-01-29 22:16:48 +0800

    fix: 🐛 got problems

--- frontend/components/form/button.vue
+++ frontend/components/form/button.vue
@@ -13,31 +13,31 @@
 const props = defineProps({
   text: {
     type: String,
-    default: ''
+    default: '',
   },
   type: {
     type: String,
-    defualt: 'button'
+    defualt: 'button',
   },
   disabled: {
     type: Boolean,
-    default: false
-  }
-})
+    default: false,
+  },
+});
 
-const { text, type, disabled } = props
+const { text, type, disabled } = props;
 
-const emits = defineEmits(['click'])
+const emits = defineEmits(['click']);
 
 function emitClick() {
-  emits('click')
+  emits('click');
 }
 </script>
 
 <style>
 .components-form-login-button {
   color: rgba(0, 0, 0, 1);
-  background-color: #FFD15C;
+  background-color: #ffd15c;
   border-radius: 10px;
   border-width: 0;
   outline: none;
